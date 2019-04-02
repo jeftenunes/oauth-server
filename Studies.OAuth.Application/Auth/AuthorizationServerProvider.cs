3b7bed336a3425e91b874fcb28b4e7ec2a42d4bc
Santos Nunes <sanje04@mes.reseau.intra>
2019-04-02 14:26:40 -0400

    [master] - OAuth2 server development

---
+++ Studies.OAuth.Application/Auth/AuthorizationServerProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Owin.Security.OAuth;
+
+namespace Studies.OAuth.Application.Auth
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            await Task.FromResult<dynamic>(new { });
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            await Task.FromResult<dynamic>(new { });
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            var user = "user".Equals(context.UserName) && "password".Equals(context.Password) ? new { UserName = "user", Info = "BLÁ BLÁ" } : null;
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+    }
+}
---
+++ Studies.OAuth.Application/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Owin;
+using System;
+using System.Linq;
+using System.Text;
+using Microsoft.Owin;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Owin.Security.OAuth;
+using Studies.OAuth.Application.Auth;
+
+namespace Studies.OAuth.Application
+{
+    public static class Extensions
+    { 
+        public static void ConfigureOAuth(this IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(1),
+                Provider = new AuthorizationServerProvider()
+            };
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
